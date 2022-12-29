4b35455601885beae6a457ebe35a2be0eebd129a
Josh Rumsey <jrumsey357@gmail.com>
2022-12-28 23:24:42 -0500

    Contact Form successfully sends email on valid submission, currently disabled for now

--- src/components/forms/contact-form/contact-form.js
+++ src/components/forms/contact-form/contact-form.js
@@ -1,7 +1,8 @@
 import { Card, CardContent, TextField, Button } from '@mui/material';
 import { nameRequired, emailRequired, messageRequired, emptyAlert, emailSent } from '../../../models/enums/dialog.enum';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import AlertDialog from '../../dialog/alert-dialog/alert-dialog';
+import emailjs from '@emailjs/browser';
 import './contact-form.css'
 
 function ContactForm() {
@@ -10,25 +11,41 @@ function ContactForm() {
   const [email, setEmailValue] = useState("");
   const [number, setNumberValue] = useState("");
   const [message, setMessageValue] = useState("");
-  const [alert, setAlert] = useState(false)
-  const [alertContent, setAlertContent] = useState(emptyAlert)
+  const [alert, setAlert] = useState(false);
+  const [alertContent, setAlertContent] = useState(emptyAlert);
+
+  // Form
+  const contactForm = useRef();
 
   // Form Functions
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    // Prevent Default
+    e.preventDefault();
+
     // Handle Validation
     let isValid = inputValidation();
     // Validation Handling
     if (!isValid) {
       return;
     }
-    // TODO: Hit External API to send email.
+
+    // Hit External API to send email.
+    // Uncomment Lines 34 through 41 to enable.
+    // emailjs.sendForm('service_9zfis6f', 'template_sbzj5dt', contactForm.current, 'jvTbsz4QSwL50Nri7')
+    //   .then(() => {
+    //       setAlertContent(emailSent);
+    //       setAlert(true);
+    //   }, (error) => {
+    //       // TODO: Return Error Dialog
+    //       console.log(error.text);
+    //   });
     // TODO: Post Message Receipt to the Database
 
     // Send Confirmation Dialog
     setAlertContent(emailSent);
     setAlert(true);
 
-    // Clear the form
+    // TODO: Find a way to clear the form
     setNameValue("");
     setEmailValue("");
     setNumberValue("");
@@ -83,19 +100,18 @@ function ContactForm() {
   }
 
   return (
-    <div className="contactFormContent">
+    <form ref={contactForm} className="contactFormContent">
       <h1 className="contactHeader">Contact</h1>
       <Card elevation={0} className="contactFormCard">
         <CardContent>
-          {/* Contact Form Works! */}
           <div>
-            <TextField className="cFormInput" label="Name" variant="outlined"
+            <TextField className="cFormInput" label="Name" variant="outlined" name="user_name"
               sx={style.textfield} onChange={(e) => {setNameValue(e.target.value)}} />
-            <TextField className="cFormInput" label="Email" variant="outlined" 
+            <TextField className="cFormInput" label="Email" variant="outlined" name="user_email"
               sx={style.textfield} onChange={(e) => {setEmailValue(e.target.value)}} />
-            <TextField className="cFormInput" label="Phone Number (Optional)" variant="outlined" 
+            <TextField className="cFormInput" label="Phone Number (Optional)" variant="outlined" name="user_number"
               sx={style.textfield} onChange={(e) => {setNumberValue(e.target.value)}} />
-            <TextField className="cFormInput" label="Message" variant="outlined" multiline maxRows={8} minRows={8}
+            <TextField className="cFormInput" label="Message" variant="outlined" multiline maxRows={8} minRows={8} name="message"
               sx={style.textfield} inputProps={style.mlineinput} onChange={(e) => {setMessageValue(e.target.value)}} />
           </div>
           <div>
@@ -105,7 +121,7 @@ function ContactForm() {
         </CardContent>
       </Card>
       <AlertDialog open={alert} handleClose={handleFormClose} content={alertContent} ></AlertDialog>
-    </div>
+    </form>
   );
 }
 
